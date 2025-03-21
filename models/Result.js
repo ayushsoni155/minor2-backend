f71d8fb746d63f3787af58cfa582da1b9bffd5c4
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2025-03-21 16:49:10 +0530

    Update Result.js

--- models/Result.js
+++ models/Result.js
@@ -6,7 +6,10 @@ const resultSchema = new mongoose.Schema({
   quizID: { type: String, required: true },
   quiz_score: { type: Number, required: true },
   time_taken: { type: Number, required: true },
-  total_correct_answers: { type: Number, required: true }
+  total_correct_answers: { type: Number, required: true },
+  subject: { type: String, required: true },
+  difficulty: { type: String, required: true },
+  quizDatetime: { type: Date, required: true }
 });
 
 module.exports = mongoose.model('Result', resultSchema);
